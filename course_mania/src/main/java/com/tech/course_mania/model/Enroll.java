43fd565870be7281f737055d0526ee485e073c4e
Praneswar G <118578571+PraneswarGanesan@users.noreply.github.com>
2024-08-05 19:05:29 +0530

    jwt example

---
+++ course_mania/src/main/java/com/tech/course_mania/model/Enroll.java
@@ -0,0 +1,34 @@
+package com.tech.course_mania.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Enroll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int enrollid;
+    private Date enrolled_date;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
+ 
---
+++ course_mania/src/main/java/com/tech/course_mania/repository/EnrollRepository.java
@@ -0,0 +1,9 @@
+package com.tech.course_mania.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tech.course_mania.model.Enroll;
+
+public interface EnrollRepository extends JpaRepository<Enroll, Integer> {
+
+}
