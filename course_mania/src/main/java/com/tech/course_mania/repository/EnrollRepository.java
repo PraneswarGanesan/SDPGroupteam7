0f582e8c4bc1af46c614b8faae2bbe2bf2cbed4a
Praneswar G <praneswarganesan@gmail.com>
2024-08-12 12:05:08 +0530

    nothing

--- course_mania/src/main/java/com/tech/course_mania/repository/EnrollRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech.course_mania.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tech.course_mania.model.Enroll;
-
-public interface EnrollRepository extends JpaRepository<Enroll, Integer> {
-
-}
